1b1f630388ee3b41068bb92a04f7e5aecba336c5
Uzair Ashraf <uzinatorcl@live.com>
2020-08-12 15:14:43 -0700

    dropped older files from pre-refactor

--- dist/search.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class Search {
-    users(token: string, apiKey: string, username: string, maxResults?: number): Promise<any>;
-    userByName(token: string, apiKey: string, username: string): Promise<any>;
-    userById(token: string, apiKey: string, id: string): Promise<any>;
-}
